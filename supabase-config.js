f62198cca94ea27ff263e2ce0fa0a34db3d07cc9
PANicAndCode <totallynotascamboi@gmail.com>
2026-04-05 18:40:06 -0500

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,3 +1,3 @@
 // Paste your Supabase project URL and anon key here
-window.SUPABASE_URL = "";
-window.SUPABASE_ANON_KEY = "";
+window.SUPABASE_URL = "https://eyzrvijtlfhnsnsvxtaj.supabase.co";
+window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV5enJ2aWp0bGZobnNuc3Z4dGFqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ1NzY0MTIsImV4cCI6MjA5MDE1MjQxMn0.WDLt-0nTyDY0ILr54zKvagaIDJo-WRVKwHL4RLrDy-s";
